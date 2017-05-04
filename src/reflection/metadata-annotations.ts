4ffad44111dce81dab07807f780301298b11a5a6
Arturo Saavedra <artu.saavedra@gmail.com>
2017-05-04 00:12:06 -0300

    Added more annotations

--- src/reflection/metadata-annotations.ts
+++ src/reflection/metadata-annotations.ts
@@ -1,9 +1,21 @@
 import "reflect-metadata";
 
+/**
+ * @class Annotation
+ * @version 0.9.0
+ * @see npm @ab/common
+ * @author arthmoeros (Arturo Saavedra) artu.saavedra@gmail.com
+ * 
+ * This Class defines constants to identify metadata keys from reflect-metadata
+ * decorators
+ * 
+ */
 export class Annotation {
 	public static readonly RestMethod: string = "RestMethod:path";
 	public static readonly RestRequestType: string = "RestResponseType:requestContentType";
 	public static readonly RestResponseType: string = "RestResponseType:responseContentType";
+	public static readonly PipeFunction: string = "PipeFunction";
+	public static readonly DefinitionFunction: string = "DefinitionFunction";
 }
 
 export function RestMethod(path){
@@ -16,4 +28,12 @@ export function RestRequestType(requestContentType){
 
 export function RestResponseType(responseContentType){
 	return Reflect.metadata(Annotation.RestResponseType, responseContentType);
+}
+
+export function PipeFunction(){
+	return Reflect.metadata(Annotation.PipeFunction, Annotation.PipeFunction);
+}
+
+export function DefinitionFunction(){
+	return Reflect.metadata(Annotation.DefinitionFunction, Annotation.DefinitionFunction);
 }
