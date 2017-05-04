c297da7a36025e7c9a17203b327b0b2e80ecce0d
Arturo Saavedra <artu.saavedra@gmail.com>
2017-05-03 21:59:40 -0300

    Added metadata handling

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from "./src/container/regex.container";
 export * from "./src/container/string.container";
 export * from "./src/utils/string-handler.util";
+export * from "./src/reflection/metadata-annotations";
