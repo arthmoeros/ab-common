8f2567e20f503607918f0cf699321aaaa942a146
arthmoeros <artu.saavedra@gmail.com>
2017-07-19 23:41:32 -0400

    moved object property locator from template engine

--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ export * from "./src/utils/number-handler.util";
 export * from "./src/reflection/metadata-annotations";
 export * from "./src/reflection/rest/content-type";
 export * from "./src/reflection/rest/rest-method.enum";
+export * from "./src/utils/object-property-locator";
