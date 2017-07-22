31116938a9eefeedc30d09c81b1e7f08d387e10a
Arturo Saavedra <artu.saavedra@gmail.com>
2017-07-22 00:09:29 -0400

    upgraded object property locator

--- src/utils/object-property-locator.ts
+++ src/utils/object-property-locator.ts
@@ -7,13 +7,15 @@
 export class ObjectPropertyLocator {
 
     /**
-     * Looks up a value within a object given a path
+     * Looks up a value within a object given a path, if a value is given, then
+     * it will be used to replace the current value
      * 
      * @param object object to inspect
      * @param path Path to lookup in the object must follow the format root.parent.children
+     * @param value value to insert
      * @throws an Error if the path points to a member of an array
      */
-    public static lookup(object: {}, path: string): any {
+    public static lookup(object: {}, path: string, value?: string): any {
         let nesting: string[] = path.split(".");
         let resolvedValue: any = null;
         let nextChild: any = object;
@@ -21,6 +23,9 @@ export class ObjectPropertyLocator {
             throw new Error("Root object is null");
         }
         for(let i = 0; i < nesting.length; i++){
+            if(value != null && (i+1) == nesting.length){
+                nextChild[nesting[i]] = value;
+            }
             nextChild = nextChild[nesting[i]];
             if (nextChild == null) {
                 resolvedValue = null;
