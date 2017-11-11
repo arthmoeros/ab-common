55e112c9fbece885f6e0f40bbd0b8895c8a92709
Arturo Saavedra <artu.saavedra@gmail.com>
2017-11-11 14:36:19 -0300

    Fixed abstract managers

--- src/abstract/blueprint.manager.ts
+++ src/abstract/blueprint.manager.ts
@@ -4,6 +4,6 @@ export abstract class BlueprintManager {
 
     public abstract getBlueprint(name: string, task: string): BlueprintContainer;
 
-    public abstract getBlueprintMaterial(blueprintName: string, materialLocation: string): Buffer;
+    public abstract getBlueprintMaterial(blueprintName: string, materialLocation: string): string;
 
 }
--- src/abstract/form.manager.ts
+++ src/abstract/form.manager.ts
@@ -2,6 +2,6 @@ export abstract class FormManager{
 
     public abstract getFormsIndex(): string[];
 
-    public abstract getForm(name: string): any;
+    public abstract getForm(name: string): string;
 
 }
